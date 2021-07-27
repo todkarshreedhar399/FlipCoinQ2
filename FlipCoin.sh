4ce31af5a3b99b2e365b82f6902b577480075fab
todkarshreedhar399 <todkarshreedhar399@gmail.com>
2021-07-27 13:10:51 +0530

    Latest Update 3

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,19 +1,31 @@
 #!/bin/bash -x
-read -p "Enter how many times you want to flip coin: " num
+
 head=0
 tail=0
-for((i=1; i<=$num; i++))
+while [ $head -le 21 ] && [ $tail -le 21 ]
 do
-result=$(($RANDOM%2))
-if [ $result -eq 1 ]
-then
-	echo "Its head"
-	head=$(($head+1))
-else
-	echo "Its tail"
-	tail=$(($tail+1))
-fi
+        result=$(($RANDOM%2))
+        if [ $result -eq 1 ]
+        then
+                head=$(($head+1))
+        else
+                tail=$(($tail+1))
+        fi
+		if [ $head -eq $tail ]
+		then
+			echo "match tie"
+		else
+			echo "No.of heads= $head"
+			echo "No.of tails= $tail"
+			if [ $head -gt $tail ]
+			then
+				echo "head won"
+				win=$(($head-$tail))
+				echo "head won by $win"
+			else
+				echo "tail won"
+				win=$(($tail-$head))
+				echo "tail won by $win"
+			fi
+		fi
 done
-echo "no.of head= $head"
-echo "no.of tail= $tail"
-
